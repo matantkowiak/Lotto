637e3af93ca715df4853e440c8f7241944062de7
Mateusz Antkowiak <mat.antkowiak90@gmail.com>
2022-09-10 12:53:13 +0200

    Change draw number

--- Lotto/Lotto.py
+++ Lotto/Lotto.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from random import shuffle
 
 def lotto_game():
     # User selectable numbers including errors
@@ -7,7 +7,7 @@ def lotto_game():
     while len(your_numb) < 6:
         try:
             numb_type = int(input("Type your number from 1 to 49:"))
-            if numb_type in range(1,49):
+            if numb_type in range(1, 50):
                 if numb_type in your_numb:
                     print("You have already chosen that number")
                 else:
@@ -19,17 +19,14 @@ def lotto_game():
     sorted_your_numb = sorted(your_numb)
     print(f"Your number: {' '.join(str(el) for el in sorted_your_numb)}")
     # selected numbers by the computer
-    drawn_numbers = []
-    for _ in range(6):
-        drown = randint(1, 49)
-        if drown not in drawn_numbers:
-            drawn_numbers.append(drown)
-    sorted_draw_number = sorted(drawn_numbers)
-    print(f"Drawn number: {' '.join(str(el) for el in sorted_draw_number)}")
+    draw_number = list(range(1,49))
+    shuffle(draw_number)
+    print(f"Draw number: {' '. join(str(el) for el in sorted(draw_number[:6]))}")
+
     # checking how many numbers are the same
     hits = 0
     for element in your_numb:
-        if element in drawn_numbers:
+        if element in draw_number[:6]:
             hits += 1
     print(f"You hits {hits}")
 
